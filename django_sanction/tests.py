df9632598d79c515acbe5ee054ce79041cfef38c
Demian Brecht <demianbrecht@gmail.com>
2012-07-31 17:11:46 -0700

    added custom user class/lookup tests

--- django_sanction/tests.py
+++ django_sanction/tests.py
@@ -46,9 +46,10 @@ settings.configure(
             scope=("email",),
         ),
     },
-    SANCTION_AUTH_FN = "django_sanction.tests.auth",
 )
 
+from django.db import models
+import django.contrib.auth.models
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.auth import authenticate
 from django.core.management import call_command
@@ -65,16 +66,6 @@ from django_sanction.backends import AuthenticationBackend
 from django_sanction.models import User
 from django_sanction import urls
 
-
-# a custom authentication method *MUST* be provided in order to
-# get the email address
-def auth(request, provider, client):
-    data = client.request("/me")
-
-    return User.objects.get_or_create(username="test",
-        email=data["email"], access_token=client.access_token,
-        provider_key=provider.name,
-        expires=data.get("expires", -1))[0]
 
 
 def render_profile(request):
@@ -93,6 +84,13 @@ urlpatterns = patterns("",
 
 class TestDefaultBackend(TestCase):
     def setUp(self):
+        settings.SANCTION_AUTH_FN = \
+            "django_sanction.tests.TestDefaultBackend.auth"
+        if hasattr(settings, "SANCTION_GET_USER_FN"):
+            delattr(settings, "SANCTION_GET_USER_FN")
+        if hasattr(settings, "SANCTION_USER_CLASS"):
+            delattr(settings, "SANCTION_USER_CLASS")
+
         self.server = subprocess.Popen(("python", "%s/test_server.py" % (
             dirname(__file__))))
 
@@ -103,11 +101,69 @@ class TestDefaultBackend(TestCase):
         except: pass
 
 
+    # a custom authentication method *MUST* be provided in order to
+    # get the email address
+    @staticmethod
+    def auth(request, provider, client):
+        data = client.request("/me")
+
+        return User.objects.get_or_create(username="test",
+            email=data["email"], access_token=client.access_token,
+            provider_key=provider.name,
+            expires=data.get("expires", -1))[0]
+
+
     def testAuthenticate(self):
         for key in settings.SANCTION_PROVIDERS:
             response = self.client.get("/o/auth/%s" % key.lower(),
                 HTTP_HOST="unittest")
             self.assertEquals(response.status_code, 302)
+
+
+    def testCode(self):
+        for key in settings.SANCTION_PROVIDERS:
+            response = self.client.get("/o/code/%s" % key.lower(),
+                HTTP_HOST="unittest", follow=True)
+
+
+class CustomUser(django.contrib.auth.models.User):
+    access_token = models.CharField(max_length=100)
+    expires = models.FloatField()
+    provider_key = models.CharField(max_length=100)
+
+
+class TestBackendCustom(TestCase):
+    def setUp(self):
+        settings.SANCTION_USER_CLASS = "django_sanction.tests.CustomUser"
+        settings.SANCTION_AUTH_FN = \
+            "django_sanction.tests.TestBackendCustom.auth"
+        settings.SANCTION_GET_USER_FN = \
+            "django_sanction.tests.TestBackendCustom.get_user"
+        self.server = subprocess.Popen(("python", "%s/test_server.py" % (
+            dirname(__file__))))
+
+
+    def tearDown(self):
+        try:
+            self.server.kill()
+        except: pass
+
+
+    # a custom authentication method *MUST* be provided in order to
+    # get the email address
+    @staticmethod
+    def auth(request, provider, client):
+        data = client.request("/me")
+
+        return CustomUser.objects.get_or_create(email=data["email"],
+            access_token=client.access_token,
+            provider_key=provider.name,
+            expires=data.get("expires", -1))[0]
+
+
+    @staticmethod
+    def get_user(user_id):
+        return CustomUser.objects.get(id=user_id)
 
 
     def testCode(self):
